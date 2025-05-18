7c4b4b68fe6e3d79278944792ba5533f9934d0cf
Alex Shutilov <oshutilov@gmail.com>
2025-05-18 17:21:15 +0300

    Refactored controller methods as helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,8 @@
 module ApplicationHelper
-
   def gravatar_for(user, options = { size: 200 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
     gravatar_url = "https://www.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block")
   end
-
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
-  def logged_in?
-    !!current_user
-  end
-
 end
